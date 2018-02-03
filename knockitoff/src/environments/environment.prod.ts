86acd8930c1020c7eedbb26ed39a0fedf600f40a
Jessica Roque <jroque.dev@gmail.com>
2018-02-03 10:43:04 -0800

    Modify for deployment to Firebase. Hide checkbox on Task History.

--- knockitoff/src/environments/environment.prod.ts
+++ knockitoff/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCj8HyNNfiPLmalubuCsV7UzdvchbaEoU8',
+    authDomain: 'knockitoff-8.firebaseapp.com',
+    databaseURL: 'https://knockitoff-8.firebaseio.com/',
+    projectId: 'knockitoff-8',
+    // storageBucket: 'gs://knockitoff-8.appspot.com/',
+    // messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
